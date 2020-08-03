932a4ddf7f107c39234c0babcdca2bf746cedbfd
Chamod <Gayashan>
2020-08-03 22:24:05 +0530

    MAD Tutorial 2

---
+++ app/src/main/java/com/example/activityone/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.activityone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView txtMsg2 = findViewById(R.id.tvMsg2);
+        txtMsg2.setText(R.string.Msg2);
+    }
+}
